dcbee681a327f44911f16a50ccb16d19c81ddefd
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-01-04 20:10:11 +0100

    :bug: (env) revoked token & removed .env through .gitignore

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'CommitMetric',
+      name: 'dev',
       script: 'ts-node',
       args: 'src/index.ts',
       instances: 1,
@@ -9,6 +9,13 @@ module.exports = {
       watch: true,
       ignore_watch: ['node_modules', 'build'],
       max_memory_restart: '1G',
+    },
+    {
+      name: 'prod',
+      script: 'node',
+      args: 'build/src/index.js',
+      instances: 1,
+      autorestart: true,
     },
   ],
 };
