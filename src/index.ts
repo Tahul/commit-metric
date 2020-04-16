0b31e741835f3a257eb048423a4909703b61997b
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-04-16 22:56:44 +0200

    :sparkles: (default-port) change default port

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,38 @@
 /**
  * IMPORTS
  */
-import * as express from 'express'
-import * as dotenv from 'dotenv'
+import * as express from 'express';
+import * as dotenv from 'dotenv';
 
 // - Metrics
-import githubMetric from './metrics/github'
-import gitlabMetric from './metrics/gitlab'
+import githubMetric from './metrics/github';
+import gitlabMetric from './metrics/gitlab';
 
-export const metrics = [githubMetric, gitlabMetric]
+export const metrics = [githubMetric, gitlabMetric];
 
 /**
  * INIT
  */
-dotenv.config()
+dotenv.config();
 
 /**
  * CONSTANTS
  */
-const app: express.Application = express()
+const app: express.Application = express();
 
 /**
  * ROUTES
  */
 app.get('/', async (req: express.Request, res: express.Response) => {
-  res.json(await getMetrics())
-})
+  res.json(await getMetrics());
+});
 
 /**
  * SERVER CREATION
  */
-app.listen(process.env.SERVER_PORT ? process.env.SERVER_PORT : 3005, () => {
-  console.log('Commit Metric is running!')
-})
+app.listen(process.env.SERVER_PORT ? process.env.SERVER_PORT : 3100, () => {
+  console.log('Commit Metric is running!');
+});
 
 /**
  * Get metrics data object from metrics array
@@ -40,17 +40,17 @@ app.listen(process.env.SERVER_PORT ? process.env.SERVER_PORT : 3005, () => {
 const getMetrics = async () => {
   let result = {
     globalCommits: 0,
-  }
+  };
 
   // Looping through each registered metrics
   for (const metric of metrics) {
-    result = Object.assign(result, await metric())
+    result = Object.assign(result, await metric());
   }
 
   // Handling global count
   for (const value of Object.values(result)) {
-    result.globalCommits = result.globalCommits + value
+    result.globalCommits = result.globalCommits + value;
   }
 
-  return result
-}
+  return result;
+};
